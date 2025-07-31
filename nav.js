db1224d662a0cac5d589842b0392c99c519db24d
mattiCohen <m0548568132@gmail.com>
2025-07-31 18:56:28 +0300

    fix the nav

--- nav.js
+++ nav.js
@@ -1,11 +1,20 @@
 function loadNav() {
+  const currentPath = window.location.pathname;
+  let prefix = '';
+
+  // Example: if the current page is inside /HomePage/, prefix is '../'
+  if (currentPath.includes('/HomePage/')) {
+    prefix = '../';
+  }
+
+  // build nav with prefix
   const navHTML = `
     <nav id="main-nav">
-      <a href="./HomePage.html">דף הבית</a>
-      <a href="./HomePage/showAll.html">📖 הצג את כל המתכונים</a>
-      <a href="../LogIn+SignUp/login.html">🔐 התחברות</a>
-      <a href="../LogIn+SignUp/signUp.html">📝 הרשמה</a>
-      <a href="../HomePage/about.html">ℹ️ אודות</a>
+      <a href="${prefix}HomePage/HomePage.html">דף הבית</a>
+      <a href="${prefix}showAll.html">📖 הצג את כל המתכונים</a>
+      <a href="${prefix}LogIn+SignUp/login.html">🔐 התחברות</a>
+      <a href="${prefix}LogIn+SignUp/signUp.html">📝 הרשמה</a>
+      <a href="${prefix}HomePage/about.html">ℹ️ אודות</a>
       <span id="admin-link-container"></span>
     </nav>
   `;
